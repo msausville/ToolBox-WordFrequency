df6c4086f69c8ebe0be1ede358ce451a1425a8bc
Meaghen Sausville <meaghen.sausville@students.olin.edu>
2017-03-19 16:50:41 -0400

    Final frequency toolbox

--- frequency.py
+++ frequency.py
@@ -1,7 +1,16 @@
-""" Analyzes the word frequencies in a book downloaded from
-Project Gutenberg """
+"""
+    Analyzes the word frequencies in a book downloaded from Project Gutenberg
+"""
 
+from collections import Counter
+# from bs4 import BeautifulSoup
+import requests
 import string
+
+# print('first test')
+# print(string.punctuation)
+
+# html = BeautifulSoup(requests.get('http://www.gutenberg.org/files/2591/2591-0.txt').text, 'lxml')
 
 
 def get_word_list(file_name):
@@ -10,7 +19,33 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    curr_line = 0
+    while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
+        curr_line += 1
+    lines = lines[curr_line+1:]
+    master_word_list = []
+
+    for line in lines:
+        # print(type(line))
+        line = line.lower()
+        if line == '\n':
+            continue
+        cut_up_line = line.strip('\n')
+        word_list = cut_up_line.split(' ')
+        master_word_list.extend(word_list)
+    return master_word_list
+        # print(line)
+    # print(master_word_list)
+
+
+    # for i in range(0, len(lines)):
+    #     line = lines[i]
+    #     word_list.extend line
+
+
+# print(listgrimms)
 
 
 def get_top_n_words(word_list, n):
@@ -21,10 +56,37 @@ def get_top_n_words(word_list, n):
     punctuation
     n: the number of words to return
     returns: a list of n most frequently occurring words ordered from most
-    frequently to least frequentlyoccurring
+    frequently to least frequently occurring
     """
-    pass
+    word_dictionary = {}
+    for word in word_list:
+        if word not in word_dictionary:
+            word_dictionary[word] = 1
+        else:
+            word_dictionary[word] += 1
+
+    word_count_obj_list = []
+    for word, count in word_dictionary.items():
+        word_count_obj_list.append(WordCountObj(word, count))
+
+    word_count_obj_list.sort(key=lambda wcobj: wcobj.count, reverse=True)
+
+    final_list =[]
+    i = 0
+    while i < n:
+        final_list.append(word_count_obj_list[i].word)
+        i += 1
+    return final_list
+
+
+class WordCountObj:
+    def __init__(self, word, count):
+        self.word = word  # string
+        self.count = count  # int
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    # print(string.punctuation)
+    top_words = get_top_n_words(get_word_list('pg32325.txt'), 60)
+    print(top_words)
+
